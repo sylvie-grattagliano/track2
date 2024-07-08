742a5d6106d8aacd819edbfcf6a4eafb63126761
sylvie <sosylvie@hotmail.com>
2024-07-08 13:00:11 +0200

    Ajout job02

---
+++ Job02/index.php
@@ -0,0 +1,30 @@
+<?php
+$str="LaPlateforme";
+$str1="!" ;
+$str2= $str . $str1 ;
+//echo "hello la plate forme"; // ceci est donc mon commentaire en ligne php; 
+/*echo "hello la plate forme"; /* ceci est mon commentaire en php un  bloc de plusieurs lignes
+sveverv
+qjiovjt
+ezue
+errter
+
+*/
+
+?>
+                                        <!--DOC HTML-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job2</title>
+</head>
+<body>
+    <H1>Job02</H1>
+    <p>Hello <?=$str2 ;?></p> 
+    <p>Hello <?=$str2 ;?></p>
+    
+</body>
+</html>
